23bf5af871308291a358f4f529ec60c6c723dde1
Dave Parker <dave.x.parker@gmail.com>
2023-05-19 17:32:45 +0100

    NeurIPS'22 code, updated to latest PRISM codebase. Now in lava package.

---
+++ prism/src/lava/PACIntervalEstimatorOptimistic.java
@@ -0,0 +1,19 @@
+package lava;
+
+import prism.Prism;
+import prism.PrismException;
+import strat.Strategy;
+
+public class PACIntervalEstimatorOptimistic extends PACIntervalEstimator
+{
+    public PACIntervalEstimatorOptimistic(Prism prism, Experiment ex)
+    {
+        super(prism, ex);
+    }
+
+    public Strategy buildStrategy() throws PrismException
+    {
+        return this.buildWeightedOptimisticStrategy(this.getEstimate(), this.ex.strategyWeight);
+    }
+}
+
---
+++ prism/src/lava/UCRL2IntervalEstimatorOptimistic.java
@@ -0,0 +1,20 @@
+package lava;
+
+import prism.Prism;
+import prism.PrismException;
+import strat.Strategy;
+
+public class UCRL2IntervalEstimatorOptimistic extends UCRL2IntervalEstimator
+{
+    public UCRL2IntervalEstimatorOptimistic(Prism prism, Experiment ex)
+    {
+        super(prism, ex);
+    }
+
+    public Strategy buildStrategy() throws PrismException
+    {
+        return this.buildWeightedOptimisticStrategy(this.getEstimate(), this.ex.strategyWeight);
+    }
+}
+
+
