e7d34f0f3ce716439d76ed24742bf1a5fabf0b2e
Dave Parker <dave.x.parker@gmail.com>
2023-07-08 10:20:08 +0100

    Implementation of memoryless randomised (MR) strategy.
    Methods exportInducedModel/exportDotFile not yet implemented.
    
    Test code in Strategy.main, but not yet used elsewhere.
    
    Added DistributionOver.toString to aid debugging.

---
+++ prism/src/strat/MRStrategy.java
@@ -0,0 +1,135 @@
+package strat;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import explicit.ConstructInducedModel;
+import explicit.Distribution;
+import explicit.DistributionOver;
+import explicit.MDPSimple;
+import explicit.Model;
+import explicit.NondetModel;
+import parser.State;
+import prism.PrismException;
+import prism.PrismLog;
+import prism.PrismNotSupportedException;
+import prism.PrismPrintStreamLog;
+import simulator.RandomNumberGenerator;
+
+/**
+ * Class to store a memoryless randomised (MR) strategy
+ * associated with an explicit engine model.
+ */
+public class MRStrategy extends StrategyExplicit<Double>
+{
+	// Probability of selecting choice indices in each state
+	protected List<Distribution<Double>> choiceProbs;
+
+	/**
+	 * Create a blank MRStrategy for a specified model.
+	 */
+	public MRStrategy(NondetModel<Double> model)
+	{
+		super(model);
+		int numStates = model.getNumStates();
+		choiceProbs = new ArrayList<>(numStates);
+		for (int i = 0; i < numStates; i++) {
+			choiceProbs.add(Distribution.ofDouble());
+		}
+	}
+
+	/**
+	 * Set the probability of selecting choice index i in state s to p
+	 */
+	public void setChoiceProbability(int s, int i, double p)
+	{
+		choiceProbs.get(s).set(i, p);
+	}
+
+	@Override
+	public Object getChoiceAction(int s, int m)
+	{
+		Distribution<Double> probs = choiceProbs.get(s);
+		return probs.isEmpty() ? Strategy.UNDEFINED : DistributionOver.create(probs, i -> model.getAction(s, i));
+	}
+
+	@Override
+	public int getChoiceIndex(int s, int m)
+	{
+		// N/A for randomised strategy
+		return -1;
+	}
+
+	@Override
+	public void exportActions(PrismLog out, StrategyExportOptions options) throws PrismException
+	{
+		List<State> states = model.getStatesList();
+		boolean showStates = options.getShowStates() && states != null;
+		int n = getNumStates();
+		for (int s = 0; s < n; s++) {
+			Object probs = getChoiceAction(s, -1);
+			if (probs != UNDEFINED) {
+				out.println((showStates ? states.get(s) : s) + "=" + probs);
+			}
+		}
+	}
+
+	@Override
+	public void exportIndices(PrismLog out, StrategyExportOptions options) throws PrismException
+	{
+		List<State> states = model.getStatesList();
+		boolean showStates = options.getShowStates() && states != null;
+		int n = getNumStates();
+		for (int s = 0; s < n; s++) {
+			Distribution<Double> probs = choiceProbs.get(s);
+			if (!probs.isEmpty()) {
+				out.println((showStates ? states.get(s) : s) + "=" + probs);
+			}
+		}
+	}
+
+	@Override
+	public void exportInducedModel(PrismLog out, StrategyExportOptions options) throws PrismException
+	{
+		throw new PrismNotSupportedException("Not yet implemented");
+		/*
+		ConstructInducedModel cim = new ConstructInducedModel();
+		cim.setMode(options.getMode()).setReachOnly(options.getReachOnly());
+		Model<Double> inducedModel = cim.constructInducedModel(model, this);
+		inducedModel.exportToPrismExplicitTra(out, options.getModelPrecision());
+
+		// NB: for reach=false (and MDPs), it would be slightly cheaper to use:
+		//Model<Value> inducedModel = model.constructInducedModel(this);
+		*/
+	}
+
+	@Override
+	public void exportDotFile(PrismLog out, StrategyExportOptions options) throws PrismException
+	{
+		throw new PrismNotSupportedException("Not yet implemented");
+		/*
+		ConstructInducedModel cim = new ConstructInducedModel();
+		cim.setMode(options.getMode()).setReachOnly(options.getReachOnly());
+		Model<Double> inducedModel = cim.constructInducedModel(model, this);
+		inducedModel.exportToDotFile(out, null, options.getShowStates(), options.getModelPrecision());
+		*/
+	}
+
+	@Override
+	public void clear()
+	{
+		choiceProbs = null;
+	}
+
+	@Override
+	public String toString()
+	{
+		return "[" + IntStream.range(0, getNumStates())
+				.mapToObj(s -> s + "=" + getChoiceActionString(s, -1))
+				.collect(Collectors.joining(",")) + "]";
+	}
+}
