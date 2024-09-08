68d8823558c588243bd1a4efbd0913a54a22f353
anonymousGRL <yannik.schnitzer@keble.ox.ac.uk>
2024-09-08 11:48:42 +0200

    loading external MR policies

--- prism/src/lava/PolicyLoader.java
+++ prism/src/lava/PolicyLoader.java
@@ -1,2 +1,48 @@
-package lava;public class PolicyLoader {
+package lava;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import explicit.MDP;
+import parser.State;
+import strat.MRStrategy;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class PolicyLoader {
+
+    ObjectMapper mapper = new ObjectMapper();
+
+    public MRStrategy loadAircraftPolicy(String policyFile, MDP<Double> model) {
+        File policyJson = new File(policyFile);
+
+        try {
+            List<List<List<List<List<Double>>>>> policyList = mapper.readValue(policyJson, List.class);
+
+            MRStrategy strat = new MRStrategy(model);
+
+            for (State s : model.getStatesList()) {
+                int x = (int) s.varValues[0];
+                int y = (int) s.varValues[1];
+                int ax = (int) s.varValues[2];
+                int ay = (int) s.varValues[3];
+
+                int state_index = model.getStatesList().indexOf(s);
+                for (int i = 0; i < model.getNumChoices(state_index); i++) {
+                    strat.setChoiceProbability(state_index, i, policyList.get(x).get(y).get(ax).get(ay).get(i));
+                }
+
+                System.out.println("State " + s + " Actions " + policyList.get(x).get(y).get(ax).get(ay));
+
+            }
+
+            System.out.println("Strategy " + strat);
+
+            return strat;
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
